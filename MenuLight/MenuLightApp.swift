b143e6e9db594cc92edc931942364ea9be27adc2
Dan Diemer <dan@headway.io>
2024-07-30 12:46:07 -0500

    Initial Commit

---
+++ MenuLight/MenuLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuLightApp.swift
+//  MenuLight
+//
+//  Created by Dan Diemer on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
